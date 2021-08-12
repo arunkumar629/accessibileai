b41305bc502ada18fb8e687b72b61648c9fe9dd9
arun <arunkumar629@gmail.com>
2021-08-12 09:44:54 -0400

    table

---
+++ t.py
@@ -0,0 +1,3 @@
+tables=int(input("Enter the table you want to print"))
+for i in range(1,11):
+	print("%d * %d = %d" % (tables,i,tables*i))
---
+++ table.py
@@ -0,0 +1,3 @@
+table=int(input("Enter the table you want to print"))
+for i in range(1,11):
+	print("%d *%d = %d" % (table,i,table*i))
